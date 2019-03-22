06af2dcd1646d21370c7b5aa6d96598a0b79c2d2
Michael <mihai.c983@gmail.com>
2019-03-22 18:46:39 +0200

    Added files

---
+++ 02_Digital Outputs/DigitalOutputs/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=DigtalOutputs1.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/DigtalOutputs1.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=digtaloutputs1.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/digtaloutputs1.tar
